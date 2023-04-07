424221a8756108b79cd520ba1f501710a348d046
HDWarnes <42464342+HDWarnes@users.noreply.github.com>
2023-04-07 17:07:52 +0200

    - added functionality to get fields from a PDF and dummy updates

--- DocumentManagementAPI/Controllers/DocumentManagemtController.cs
+++ DocumentManagementAPI/Controllers/DocumentManagemtController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,20 +16,60 @@ namespace DocumentManagementAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            // Open an existing PDF document
+            PdfReader reader = new PdfReader("input.pdf");
+            PdfStamper stamper = new PdfStamper(reader, new FileStream("output.pdf", FileMode.Create));
+
+            // Get the PDF form fields
+            AcroFields formFields = stamper.AcroFields;
+
+            // Get the field names
+            List<string> fieldNames = new List<string>(formFields.Fields.Keys);
+
+            // Close the PDF document
+            reader.Close();
+
+            // Output the field names
+           
+            return fieldNames;
         }
 
         // GET: api/DocumentManagemt/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public List<string> Get(int id)
         {
-            return "Cake";
+            // Open an existing PDF document
+            PdfReader reader = new PdfReader("input.pdf");
+            PdfStamper stamper = new PdfStamper(reader, new FileStream("output.pdf", FileMode.Create));
+
+            // Get the PDF form fields
+            AcroFields formFields = stamper.AcroFields;
+
+            // Get the field names
+            List<string> fieldNames = new List<string>(formFields.Fields.Keys);
+
+            // Close the PDF document
+            reader.Close();
+
+            // Output the field names
+
+            return fieldNames;
         }
 
         // POST: api/DocumentManagemt
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            PdfReader reader = new PdfReader("input.pdf");
+            PdfStamper stamper = new PdfStamper(reader, new FileStream("output.pdf", FileMode.Create));
+
+            // Get the PDF form fields
+            AcroFields formFields = stamper.AcroFields;
+            // Modify the value of a form field
+            formFields.SetField("Given Name Text Box", "New Value");
+
+            // Close the PDF stamper
+            stamper.Close();
         }
 
         // PUT: api/DocumentManagemt/5
